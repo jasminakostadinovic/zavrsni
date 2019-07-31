6e33453ad69a73a25e5345abb35066f6538dbd6e
jasminakostadinovic <jasmina.kostadinovic26@gmail.com>
2019-08-01 01:59:57 +0200

    izmenjeno

--- database.php
+++ database.php
@@ -1,17 +1,10 @@
 <?php
 
-
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
-
-    
     try {
         $connection = new PDO(
             'mysql:host=127.0.0.1;dbname=blog',
             'root',
-            'root'
+            ''
         );
         $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     } catch(PDOException $e){
@@ -20,5 +13,4 @@ error_reporting(E_ALL);
 
     
 ?>
-
 
