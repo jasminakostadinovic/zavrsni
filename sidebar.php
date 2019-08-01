8a9711774d82cf96a76bc9bcc4e12ea906564775
jasminakostadinovic <jasmina.kostadinovic26@gmail.com>
2019-08-01 11:04:33 +0200

    sidebar radi

--- sidebar.php
+++ sidebar.php
@@ -1,5 +1,8 @@
 <?php
-    $sql = "SELECT posts.Id, posts.Title, posts.Created_at, posts.Author, posts.Body
+
+    require('database.php');
+
+    $sql = "SELECT posts.Id as Id, posts.Title as Title, posts.Created_at as Created_at, posts.Author as Author, posts.Body as Body
     FROM posts ORDER BY Created_at DESC 
     LIMIT 5";
     $statement = $connection->prepare($sql);
@@ -9,34 +12,17 @@
 ?>
 
 
+
 <aside class="col-sm-3 ml-sm-auto blog-sidebar">
-            <div class="sidebar-module sidebar-module-inset">
-                <h4>About</h4>
-                <p>Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
-            </div>
-            <div class="sidebar-module">
-                <h4>Archives</h4>
-                <ol class="list-unstyled">
-                    <li><a href="#">March 2014</a></li>
-                    <li><a href="#">February 2014</a></li>
-                    <li><a href="#">January 2014</a></li>
-                    <li><a href="#">December 2013</a></li>
-                    <li><a href="#">November 2013</a></li>
-                    <li><a href="#">October 2013</a></li>
-                    <li><a href="#">September 2013</a></li>
-                    <li><a href="#">August 2013</a></li>
-                    <li><a href="#">July 2013</a></li>
-                    <li><a href="#">June 2013</a></li>
-                    <li><a href="#">May 2013</a></li>
-                    <li><a href="#">April 2013</a></li>
-                </ol>
-            </div>
-            <div class="sidebar-module">
-                <h4>Elsewhere</h4>
-                <ol class="list-unstyled">
-                    <li><a href="#">GitHub</a></li>
-                    <li><a href="#">Twitter</a></li>
-                    <li><a href="#">Facebook</a></li>
-                </ol>
-            </div>
+    <div class="sidebar-module sidebar-module-inset">
+        <h4>Latest posts</h4>
+        <ul>
+            <?php foreach($posts as $post){ ;?>
+                <li>
+                    <a href="single-post.php?post_id=<?php echo($post['Id']) ?>"><p><?php echo ($post['Title']); ?><p></a>
+                </li>
+            <?php } ;?>
+        </ul>
+    </div>
+            
 </aside>
